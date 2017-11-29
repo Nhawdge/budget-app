6533133a36b63ed8db09508235d55fba74398470
John Pavek <JohnQPavek@gmail.com>
2017-11-28 23:46:26 -0600

    Added categories

--- viewmodels/globalviewmodels.js
+++ viewmodels/globalviewmodels.js
@@ -1,17 +1,17 @@
 var AppConfig = {
     Categories: [
-        { name: 'Income', Subcategories: [{ name: 'Ashley 1' }, { name: 'John 1' }, { name: 'Ashley 2' }, { name: 'John 2' }] },
-        { name: 'Savings', Subcategories: [{ name: 'Emergency' }] },
-        { name: 'Housing', Subcategories: [{ name: 'Mortage' }] },
-        { name: 'Utilities', Subcategories: [{ name: 'Internet' }, { name: 'Water' }, { name: 'Cell' }, { name: 'Electricity' }, { name: 'Gas' }, { name: 'Trash' }, { name: 'Netflix' }] },
-        { name: 'Food', Subcategories: [{ name: 'Groceries' }, { name: 'Restaurants', InEnvelope: true }] },
-        { name: 'Clothing', Subcategories: [{ name: 'Adults' }, { name: 'Kid' }] },
-        { name: 'Transportation', Subcategories: [{ name: 'Gas' }, { name: 'Repair' }, { name: 'Registration' }] },
-        { name: 'Medical', Subcategories: [{ name: 'Vitamins' }, { name: 'Gym' }] },
-        { name: 'Insurance', Subcategories: [{ name: 'Auto' }, { name: 'Life' }] },
-        { name: 'Personal', Subcategories: [{ name: 'Baby Supplies' }, { name: 'Baby Care' }, { name: 'Toilet' }, { name: 'Gifts' }, { name: 'Subscriptions' }, { name: 'Him' }, { name: 'Her' }, { name: 'Pet' }] },
-        { name: 'Recreation', Subcategories: [{ name: 'Entertainment' }] },
-        { name: 'Debts', Subcategories: [{ name: 'Ash Loans' }, { name: 'John Loans' }, { name: 'A Misc' }, { name: 'J Misc' }] },
+        { name: 'Income', subcategories: [{ name: 'Ashley 1' }, { name: 'John 1' }, { name: 'Ashley 2' }, { name: 'John 2' }] },
+        { name: 'Savings', subcategories: [{ name: 'Emergency' }] },
+        { name: 'Housing', subcategories: [{ name: 'Mortage' }] },
+        { name: 'Utilities', subcategories: [{ name: 'Internet' }, { name: 'Water' }, { name: 'Cell' }, { name: 'Electricity' }, { name: 'Gas' }, { name: 'Trash' }, { name: 'Netflix' }] },
+        { name: 'Food', subcategories: [{ name: 'Groceries' }, { name: 'Restaurants', InEnvelope: true }] },
+        { name: 'Clothing', subcategories: [{ name: 'Adults' }, { name: 'Kid' }] },
+        { name: 'Transportation', subcategories: [{ name: 'Gas' }, { name: 'Repair' }, { name: 'Registration' }] },
+        { name: 'Medical', subcategories: [{ name: 'Vitamins' }, { name: 'Gym' }] },
+        { name: 'Insurance', subcategories: [{ name: 'Auto' }, { name: 'Life' }] },
+        { name: 'Personal', subcategories: [{ name: 'Baby Supplies' }, { name: 'Baby Care' }, { name: 'Toilet' }, { name: 'Gifts' }, { name: 'Subscriptions' }, { name: 'Him' }, { name: 'Her' }, { name: 'Pet' }] },
+        { name: 'Recreation', subcategories: [{ name: 'Entertainment' }] },
+        { name: 'Debts', subcategories: [{ name: 'Ash Loans' }, { name: 'John Loans' }, { name: 'A Misc' }, { name: 'J Misc' }] },
     ]
 };
 
--- viewmodels/homeviewmodel.js
+++ viewmodels/homeviewmodel.js
@@ -1,10 +1,12 @@
 function Category(data) {
+    var self = this;
     if (data == null) data = {};
 
+    console.log(data);
     self.id = ko.observable(data.id);
     self.name = ko.observable(data.name);
     self.subcategories = ko.observableArray();
-    for ( i in data.subcategories ) {
+    for (i in data.subcategories) {
         self.subcategories.push(data.subcategories[i]);
     }
 }
@@ -13,8 +15,13 @@ function Category(data) {
 function HomeViewModel() {
     var self = this;
 
-    self.Load = function () {
+    self.categories = ko.observableArray();
 
+    self.Load = function () {
+        for (i in AppConfig.Categories) {
+            self.categories.push(new Category(AppConfig.Categories[i]));
+        }
+        console.log(ko.toJS(self.categories));
     }
 
     self.Load();
